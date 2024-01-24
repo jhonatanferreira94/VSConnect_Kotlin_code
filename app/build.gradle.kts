2e2aaee714ae4c4dd941d6f738dd785490ed0f1a
Jhonatan <jhonjhonpf@hotmail.com>
2024-01-23 21:51:16 -0300

    Atualizacap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,10 @@ android {
 
 dependencies {
 
+    //dependencia que arredonda imagem - circleimageview
+    implementation("de.hdodenhof:circleimageview:3.1.0")
+
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -48,4 +52,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
--- app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/senai/vsconnect_kotlin/LoginActivity.kt
@@ -7,19 +7,26 @@ import com.senai.vsconnect_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    //É uma propriedade privada  como o nome binding do tipo ActivityLoginBinding
     private lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        //Atribui à variável binding um objeto que contém referências (propriedades) aos elementos definidos no layout
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        binding.button.setOnClickListener {
+
+        //setOnClickListener é um ouvinte de clique
+        //Ou seja, quando clicar no botão entrar irá cair nesse bloco
+        binding.btnEntrar.setOnClickListener {
+            //variável mainIntent com a intenção de sair da LoginActivity e ir para MainActivity
             val mainIntent = Intent(this@LoginActivity, MainActivity::class.java)
+            //executa a intenção armazenada na variável mainIntent
             startActivity(mainIntent)
+            //finaliza a LoginActivity
             finish()
         }
-
         setContentView(binding.root)
+
     }
 }
